c3b99204937ce946ece52823859be8fbcac4e39d
junhp1234 <junhp12345@gmail.com>
2022-11-16 16:36:02 +0900

    Idle 컴포넌트 추

---
+++ src/IDLE.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import axios from "axios";
+
+function Idle() {
+
+    return (
+        <main>
+            idle page
+        </main>
+    );
+}
+
+export default Idle;
