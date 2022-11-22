7d5787501d6289fd755f53b6e655dceb39ef731f
junhp1234 <junhp12345@gmail.com>
2022-11-23 02:51:03 +0900

    Backend image display example Reference : https://developer0809.tistory.com/104

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import axios from "axios";
 
 function App() {
     const [users, setUsers] = useState(null);
+    const [images, setImages] = useState(null);
+
+    const backend_url = "http://203.250.32.193:8000"
 
     const getpeoplelist = () => {
         axios
-            .get("http://203.250.32.193:8000/sampleapi/people/")
+            .get(backend_url + "/sampleapi/peoples/")
             .then(res => {
                 setUsers(res.data)
-                setLoading(true)
+                console.log(res.data)
+            })
+            .catch((err) => console.log(err));
+    };
+
+    const getimagelist = () => {
+        axios
+            .get(backend_url + "/sampleapi/images/")
+            .then(res => {
+                setImages(res.data)
+                console.log(res.data)
             })
             .catch((err) => console.log(err));
     };
@@ -22,7 +35,7 @@ function App() {
             species: 1
         };
         axios
-            .post("http://203.250.32.193:8000/sampleapi/people/", person)
+            .post(backend_url + "/sampleapi/people/", person)
             .then((res) => {
                 console.log(res)
             })
@@ -38,13 +51,30 @@ function App() {
             <button onClick={insertpeople}>
                 POST
             </button>
+            <button onClick={getimagelist}>
+                Image GET
+            </button>
             <div>
                 example
             </div>
             <div>
+                <h2>User result</h2>
                 <div>{ users && users[0].name }</div>
                 <div>{ users && users[0].birth_year }</div>
                 <div>{ users && users[0].eye_color }</div>
+            </div>
+            <div>
+                <h2>Image result</h2>
+                { images && images.map((list, i) => (
+                    <div key={i}>
+                        <div>
+                            <img src={backend_url + list.image} />
+                            <div>
+                                <h4>{list.title}</h4>
+                            </div>
+                        </div>
+                    </div>
+                ))}
             </div>
         </main>
     );
